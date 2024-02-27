5447a98d9327cfa83dc74944a1f4f6972e66c5db
Massimiliano Arena <milikkan5000@gmail.com>
2024-02-27 12:23:44 +0100

    Inizio Progetto

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
+import Box from '@mui/material/Box';
 
-function App() {
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
+const App = () => {
+    
+  const [list, setList] = useState<string[]>([]);
+  const [item, setItem] = useState<string>('');
+
+  function addToList(item: string) {
+    setList([...list, item]);
+  }
+
+  function removeFromList(item: string) {
+    setList(list.filter((i) => i !== item));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+     <div style={{backgroundColor:'#cfe8fc', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+        <h1 style={{textAlign:'center'}}>Lista di cose da fare</h1>
+        <TextField 
+            label="Aggiungi alla lista"
+            id="outlined-size-small"
+            sx={{height: '45px', width: '300px'}}
+            size="small" type="text" onChange={(e) => setItem(e.target.value)} />
+        <Button variant="outlined" onClick={() => addToList(item)}>Aggiungi</Button>
+        <ul>
+          {list.map((item, itemIndex) => (
+            <li key={itemIndex}>
+              {item}
+              <Button variant="outlined" color="error" onClick={() => removeFromList(item)}>Rimuovi</Button>
+              <Checkbox {...label} />
+            </li>
+          ))}
+        </ul>
+      </Box>
+     </div>
+    );
+};
 
 export default App;
